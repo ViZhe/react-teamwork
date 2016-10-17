19ae28a3e55c7b5b3c36bbbb75912abb36144e7d
ViZhe <vizhe.cat@ya.ru>
2016-10-17 20:52:22 +0300

    add useStrict

--- source/index.jsx
+++ source/index.jsx
@@ -1,12 +1,15 @@
 
 import React from 'react'
 import ReactDOM from 'react-dom'
+import {useStrict} from 'mobx'
 import {Provider} from 'mobx-react'
 import {Router, browserHistory} from 'react-router'
 
 import * as stores from './stores'
 import routes from './routes'
 
+
+useStrict(true)
 
 ReactDOM.render(
   <Provider {...stores} >
