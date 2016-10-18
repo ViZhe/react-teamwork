a8dc1fd5c72ee11e0671602ff2ee63337b9c2398
ViZhe <vizhe.cat@ya.ru>
2016-10-18 19:32:24 +0300

    update App component

--- source/components/App/index.jsx
+++ source/components/App/index.jsx
@@ -1,10 +1,11 @@
 
 import React from 'react'
+import {Link} from 'react-router'
 
 
 const App = ({children}) => (
   <div>
-    <h1>App</h1>
+    <h1>App <Link to='/' >home</Link></h1>
     {children}
   </div>
 )
