21c399e3b14e7afeef995f3b76fbfb6f5b02da26
ViZhe <vizhe.cat@ya.ru>
2016-10-19 20:46:07 +0300

    update entityStore

--- source/stores/entityStore/index.js
+++ source/stores/entityStore/index.js
@@ -4,18 +4,9 @@ import {observable, action, map} from 'mobx'
 
 class EntityStore {
   @observable entities = map({})
-  @observable ids = map({})
 
   @action syncEntities = (key, entity) => {
     this.entities.get(key).set(entity.id, entity)
-    this.ids.get(key).push(entity.id)
-  }
-
-  @action setEntities = (entities) => {
-    const entitiesKeys = Object.keys(entities)
-    entitiesKeys.forEach((entityKey) => {
-      this.entities.set(entityKey, map(entities[entityKey]))
-    })
   }
 
   @action mergeEntities = (key, entities) => {
@@ -27,17 +18,11 @@ class EntityStore {
     entitiesKeys.forEach((entityKey) => {
       this.entities.get(key).set(entityKey, entities[entityKey])
     })
-    this.ids.set(key, entitiesKeys)
   }
 
   getEntitiesByKey(key) {
     const entities = this.entities.get(key)
     return entities ? entities.toJS() : {}
-  }
-
-  getIdsByKey(key) {
-    const ids = this.ids.get(key)
-    return ids ? ids.toJS() : []
   }
 }
 
