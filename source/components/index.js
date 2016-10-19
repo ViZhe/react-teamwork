9a0898efa684aff337cbe43f37f2fa9c965fba21
ViZhe <vizhe.cat@ya.ru>
2016-10-19 20:51:09 +0300

    enable Timer component

--- source/components/index.js
+++ source/components/index.js
@@ -1,5 +1,6 @@
 
 export App from './App'
+export Timer from './Timer'
 export Boards from './Boards/presenter'
 export BoardsAdd from './BoardsAdd/presenter'
 export BoardIndex from './BoardIndex/presenter'
