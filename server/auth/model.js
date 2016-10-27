d8177416cde673babe29904dcfeb9690c48220b8
ViZhe <vizhe.cat@ya.ru>
2016-10-27 23:14:57 +0300

    add auth model

---
+++ server/auth/model.js
@@ -0,0 +1,18 @@
+
+import mongoose, {Schema} from 'mongoose'
+
+
+const UserSchema = new Schema({
+  email: {
+    type: String,
+    unique: true,
+    required: true
+  },
+  password: {
+    type: String,
+    required: true
+  }
+})
+
+
+export default mongoose.model('User', UserSchema)
