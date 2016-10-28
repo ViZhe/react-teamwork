9044670a43d2d255705df324b7df52960307f201
ViZhe <vizhe.cat@ya.ru>
2016-10-28 19:08:41 +0300

    add login route

--- server/auth/routes.js
+++ server/auth/routes.js
@@ -5,13 +5,25 @@ import {Router} from 'express'
 const routes = (passport) => {
   const router = new Router()
 
+  router.post('/login', (req, res, next) => (
+    passport.authenticate('localLogin', (err, user, info) => {
+      if (err) {
+        return res.status(201).json({where: 'routesLoginErr', err, info})
+      }
+      if (!user) {
+        return res.status(201).json({where: 'routesLoginUser', user, err, info})
+      }
+      return res.status(200).json(user)
+    })(req, res, next)
+  ))
+
   router.post('/signup', (req, res, next) => (
     passport.authenticate('localSignup', (err, user, info) => {
       if (err) {
-        return res.status(201).json({where: 'routesErr', err, info})
+        return res.status(201).json({where: 'routesSingupErr', err, info})
       }
       if (!user) {
-        return res.status(201).json({where: 'routesUser', user, info, err})
+        return res.status(201).json({where: 'routesSingupUser', user, info, err})
       }
       return res.status(200).json(user)
     })(req, res, next)
