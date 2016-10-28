cd3694edfcbcc672bfbc3c2257f8eace759db9a7
ViZhe <vizhe.cat@ya.ru>
2016-10-28 19:10:46 +0300

    add strategy for login

--- server/auth/passport.js
+++ server/auth/passport.js
@@ -1,27 +1,45 @@
 
 import passport from 'passport'
-import LocalStrategy from 'passport-local'
-import bcrypt from 'bcrypt'
+import {Strategy} from 'passport-local'
 
 import User from './model'
 
 
-passport.use('localSignup', new LocalStrategy.Strategy({
+passport.use('localLogin', new Strategy({
   usernameField: 'email',
   passwordField: 'password',
   passReqToCallback: true
 }, (req, email, password, done) => {
   User.findOne({email}, (err, user) => {
     if (err) {
-      return done(null, null, {where: 'indexErr', err, user})
+      return done(null, null, {where: 'passLoginErr', err, user})
+    }
+    if (!user) {
+      return done(null, null, {where: 'passLoginUser', err, user})
+    }
+    if (!user.validPassword(password)) {
+      return done(null, null, {where: 'passLoginCompare', err, user})
+    }
+    return done(null, user)
+  })
+}))
+
+passport.use('localSignup', new Strategy({
+  usernameField: 'email',
+  passwordField: 'password',
+  passReqToCallback: true
+}, (req, email, password, done) => {
+  User.findOne({email}, (err, user) => {
+    if (err) {
+      return done(null, null, {where: 'passSingupErr', err, user})
     }
     if (user) {
-      return done(null, null, {where: 'indexUser', err, user})
+      return done(null, null, {where: 'passSingupUser', err, user})
     }
 
     const newUser = new User()
     newUser.email = email
-    newUser.password = bcrypt.hashSync(password, bcrypt.genSaltSync(8), null)
+    newUser.password = newUser.encryptPassword(password)
 
     return newUser.save((error, savedUser) => (
       done(error, savedUser)
