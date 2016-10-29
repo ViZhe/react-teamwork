cf6b1414b717b72c8ad30baa0480a26ee63236e1
ViZhe <vizhe.cat@ya.ru>
2016-10-29 17:27:37 +0300

    add billet for auth actions

---
+++ source/actions/auth/index.js
@@ -0,0 +1,11 @@
+
+export const singUp = (data) => {
+  console.log('singUp', data)
+}
+export const singIn = (data) => {
+  console.log('singIn', data)
+}
+
+export const singOut = (data) => {
+  console.log('singOut', data)
+}
--- source/actions/index.js
+++ source/actions/index.js
@@ -1,3 +1,4 @@
 
+export * from './auth'
 export * from './timer'
 export * from './entity'
