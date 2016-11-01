ded0f3fd12d6671c6292d9daaccd8081055f71cf
ViZhe <vizhe.cat@ya.ru>
2016-11-01 21:10:09 +0300

    rewrite with promises

--- server/auth/passport.js
+++ server/auth/passport.js
@@ -14,56 +14,54 @@ const strategyOption = {
 passport.use('localSignIn', new Strategy(
   strategyOption,
   (req, email, password, done) => {
-    User.findOne({email}, (err, user) => {
-      if (err) {
-        return done(err)
-      }
-      if (!user) {
-        return done(null, false, {
-          message: 'Wrong credentials'
-        })
-      }
-      if (!user.validPassword(password)) {
-        return done(null, false, {
-          message: 'Wrong credentials'
-        })
-      }
+    User.findOne({email})
+      .then((user) => {
+        if (!user) {
+          return done(null, false, {
+            message: 'Wrong credentials'
+          })
+        }
+        if (!user.validPassword(password)) {
+          return done(null, false, {
+            message: 'Wrong credentials'
+          })
+        }
 
-      const {_id: id} = user
-      return done(null, {id})
-    })
+        const {_id} = user
+        return done(null, {_id})
+      })
+      .catch(done)
   }
 ))
 
 passport.use('localSignUp', new Strategy(
   strategyOption,
   (req, email, password, done) => {
-    User.findOne({email}, (err, user) => {
-      if (err) {
-        return done(err)
-      }
-      if (user) {
-        return done(null, false, {
-          message: 'Email already exists'
-        })
-      }
-
-      const newUser = new User()
-      newUser.email = email
-      newUser.password = newUser.encryptPassword(password)
-
-      return newUser.save((error, {_id: id}) => {
-        if (err) {
-          return done(err)
+    User.findOne({email})
+      .then((user) => {
+        if (user) {
+          return done(null, false, {
+            message: 'Email already exists'
+          })
         }
-        return done(null, {id})
+
+        const newUser = new User()
+        newUser.email = email
+        newUser.password = newUser.encryptPassword(password)
+
+        return newUser.save((err, {_id}) => {
+          if (err) {
+            return done(err)
+          }
+          return done(null, {_id})
+        })
       })
-    })
+      .catch(done)
   }
 ))
 
-passport.serializeUser(({id}, done) => {
-  done(null, id)
+passport.serializeUser(({_id}, done) => {
+  done(null, _id)
 })
 
 passport.deserializeUser((id, done) => {
