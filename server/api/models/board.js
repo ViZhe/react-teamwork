d89ec581179284af29a0af46f1761b25867398a1
ViZhe <vizhe.cat@ya.ru>
2016-11-03 00:08:50 +0300

    add virtual & toJSON for board schema

--- server/api/models/board.js
+++ server/api/models/board.js
@@ -1,17 +1,32 @@
 
 import mongoose, {Schema} from 'mongoose'
 
+import {hideProps} from '../../utils/mongoose'
 
-const userSchema = new Schema({
+
+const boardSchema = new Schema({
   owner_id: {
-    type: String,
+    type: Schema.Types.ObjectId,
+    ref: 'User',
     required: true
   },
   name: {
     type: String,
     required: true
   }
+}, {
+  toJSON: {
+    versionKey: false,
+    virtuals: true,
+    transform: hideProps
+  }
+})
+
+boardSchema.virtual('columns', {
+  ref: 'Column',
+  localField: '_id',
+  foreignField: 'board_id'
 })
 
 
-export default mongoose.model('Board', userSchema)
+export default mongoose.model('Board', boardSchema)
