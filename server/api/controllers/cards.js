63896bf4a2da3af08ec027fea2af117846018384
ViZhe <vizhe.cat@ya.ru>
2016-11-05 19:45:43 +0300

    add example api for cards

--- server/api/controllers/cards.js
+++ server/api/controllers/cards.js
@@ -1,26 +1,13 @@
 
 
 export default {
-  list: (req, res) => {
-    res.status(200).json([
-      {
-        id: 'cardId1',
-        column_id: 'columnId1',
-        name: 'Первая карточка',
-        position: 1
-      },
-      {
-        id: 'cardId2',
-        column_id: 'columnId1',
-        name: 'Вторая карточка',
-        position: 2
-      },
-      {
-        id: 'cardId3',
-        column_id: 'columnId2',
-        name: 'Третья карточка',
-        position: 1
-      }
-    ])
+  add: (req, res) => {
+    console.log('cards', 'add', req, res)
+  },
+  update: (req, res) => {
+    console.log('cards', 'update', req, res)
+  },
+  delete: (req, res) => {
+    console.log('cards', 'delete', req, res)
   }
 }
