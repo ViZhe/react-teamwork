cbab51c913db705dfd8293cba3555866596bcac1
ViZhe <vizhe.cat@ya.ru>
2016-11-05 22:22:36 +0300

    enable column api

--- server/api/index.js
+++ server/api/index.js
@@ -14,7 +14,6 @@ router.all('*',
   authMiddleware.isAuthenticated
 )
 
-router.get('/columns', columns.list)
 router.get('/user', user.get)
 
 router.get('/boards', boards.list)
@@ -22,6 +21,10 @@ router.post('/boards', boards.add)
 router.get('/boards/:id', boards.item)
 router.put('/boards/:id', boards.update)
 router.delete('/boards/:id', boards.delete)
+
+router.post('/columns', columns.add)
+router.put('/columns/:id', columns.update)
+router.delete('/columns/:id', columns.delete)
 
 router.post('/cards', cards.add)
 router.put('/cards/:id', cards.update)
